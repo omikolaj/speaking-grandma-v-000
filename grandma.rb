26b68f0e28d435ce0c3ec55de80b3685ec9b8699
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-09-30 17:19:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,9 @@
 
 def speak_to_grandma(phrase)
   if(phrase == "I LOVE YOU GRANDMA!")
-    "NO, NOT SINCE 1938!"
-  elsif(phrase == phrase.upcase)
     "I LOVE YOU TOO PUMPKIN!"
+  elsif(phrase == phrase.upcase)
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
